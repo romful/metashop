fd30d19a94c82e14fd10e9ad144dcc6ba7637256
Rom <romful@gmail.com>
2021-12-17 19:13:49 +0100

    logos partners

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,6 +1,24 @@
 <template>
   <footer class="container px-4 py-12 mx-auto border-solid border-gray-300">
   <div class="grid grid-cols-2 gap-10 mb-3 md:grid-cols-3 lg:grid-cols-11 lg:gap-20">
+<h1 class="mb-3 text-3xl font-semibold leading-tight text-center text-gray-900 md:text-xl">Join the 12,000+ businesses using Petra</h1>
+    <p class="mb-16 text-medium text-center text-gray-600">Our waiting list for early bird clients is growing, get your spot and we'll update you soon!</p>
+    <div class="grid grid-cols-2 gap-10 justify-center lg:grid-cols-4">
+      <div class="flex items-center justify-center">
+        <img src="~/assets/images/logos/en.svg" alt="Todoist Logo" class="block object-contain h-12" />
+      </div>
+      <div class="flex items-center justify-center">
+        <img src="~/assets/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
+      </div>
+      <div class="flex items-center justify-center">
+        <img src="~/assets/images/logos/y.svg" alt="Typeform Logo" class="block object-contain h-12" />
+      </div>
+      <div class="flex items-center justify-center">
+        <img src="~/assets/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
+      </div>
+    </div>
+</div>
+  <div class="grid grid-cols-2 gap-10 mb-3 md:grid-cols-3 lg:grid-cols-11 lg:gap-20">
     <div class="col-span-3">
       <a href="#" class="flex items-center">
         <Logo />
@@ -35,6 +53,7 @@
       <a class="flex mb-3 text-sm font-medium text-gray-100 transition md:mb-2 hover:text-gray-200" href="#">Terms</a>
     </nav>
   </div>
+
   <div class="flex flex-col items-start justify-between pt-10 mt-10 border-t border-gray-100 md:flex-row md:items-center">
     <p class="mb-0 text-xs text-left text-gray-100 md:mb-0">Copyright &copy; 2021 Metashop RealEstate</p>
   </div>
