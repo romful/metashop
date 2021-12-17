f4a51607dedb235128c757e02dd5e15a223744bd
Rom <romful@gmail.com>
2021-12-17 19:16:19 +0100

    logos update

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -5,16 +5,16 @@
     <p class="mb-16 text-medium text-center text-gray-600">Our waiting list for early bird clients is growing, get your spot and we'll update you soon!</p>
     <div class="grid grid-cols-2 gap-10 justify-center lg:grid-cols-4">
       <div class="flex items-center justify-center">
-        <img src="~/assets/images/logos/en.svg" alt="Todoist Logo" class="block object-contain h-12" />
+        <img src="~/static/images/logos/en.svg" alt="Todoist Logo" class="block object-contain h-12" />
       </div>
       <div class="flex items-center justify-center">
-        <img src="~/assets/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
+        <img src="~/static/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
       </div>
       <div class="flex items-center justify-center">
-        <img src="~/assets/images/logos/y.svg" alt="Typeform Logo" class="block object-contain h-12" />
+        <img src="~/static/images/logos/y.svg" alt="Typeform Logo" class="block object-contain h-12" />
       </div>
       <div class="flex items-center justify-center">
-        <img src="~/assets/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
+        <img src="~/static/images/logos/vb.svg" alt="Slack Logo" class="block object-contain h-12" />
       </div>
     </div>
 </div>
