7eb8ad011425c23e35f40673fae19aeebecafc4e
bot11x11 <procom@mail.ru>
2021-12-17 18:20:17 +0700

    home.png

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="home">
-    <div class="bg-[url('/img/welcome.png')] py-24 md:py-36 mx-auto w-full">
+    <div class="bg-[url('/images/uploads/home.png')] py-24 md:py-36 mx-auto w-full">
       <h1 class="metaverse-real-estat text-center text-white text-4xl sm:text-5xl md:text-6xl font-bold">Metaverse Real Estate</h1>
       <h2 class="welcome-to-the-futur text-center text-white text-2xl sm:text-2xl md:text-2xl">Welcome to the future of property assets</h2>
     </div>
