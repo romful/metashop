46c9b22fc679dee068bf27469ea6d70729822a1f
bot11x11 <procom@mail.ru>
2021-12-20 17:27:33 +0700

    blocks for different widths

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="home">
 
-    <div class="py-32 md:py-32 w-full bg-center bg-contain bg-black bg-no-repeat" style="background-image: url(/images/home.png); min-height: 425px;">
+    <div class="py-32 md:py-32 w-full bg-center bg-cover bg-black bg-no-repeat" style="background-image: url(/images/home.png); min-height: 425px;">
       <h1 class="metaverse-real-estat text-center text-white text-4xl sm:text-5xl md:text-6xl font-bold">Metaverse Real Estate</h1>
       <h2 class="welcome-to-the-futur text-center text-white text-2xl sm:text-2xl md:text-2xl">Welcome to the future of property assets</h2>
     </div>
 
-    <div class="rectangle px-24 py-20 w-full mx-auto my-20 bg-right-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Rectangle.png); max-width: 1003px">
+    <div class="rectangle py:12 md:py-20 px-16 md:pl-24 md:pr-64 w-full mx-auto my-20 bg-right-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Rectangle.png); max-width: 1003px">
       <div class="title text-white text-3xl sm:text-4xl md:text-5xl leading-tight font-bold mb-10">
         Available for your game,<br />
         experience and land
       </div>
-      <div class="text text-white" style="padding-right: 45%;">
+      <div class="text text-white md:mr-64">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec semper lacus. Nulla facilisi. In hac habitasse platea dictumst. Us et netus et malesuada fames ac turpis egestas</p>
         <p>Proin auctor enim et mi vestibulum pretium.</p>
       </div>
     </div>
 
-    <div class="grid grid-cols-2 mx-auto my-20 gap-24 justify-center lg:grid-cols-3" style="max-width: 1003px">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-20 lg:mx-auto my-20 gap-24 justify-center" style="max-width: 1003px">
       <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
         <img class="w-full" src="~/static/images/asset1.svg" alt="Sunset in the mountains" />
         <div class="px-6 py-4">
@@ -28,7 +28,7 @@
             </div>
             <div>
             	<div class="text-xs text-gray-600 text-right">Top bid</div>
-            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" /> 100.00</div>
+            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" />100.00</div>
             </div>
           </div>
           <hr class="my-3" />
@@ -45,7 +45,7 @@
             </div>
             <div>
             	<div class="text-xs text-gray-600 text-right">Top bid</div>
-            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" /> 100.00</div>
+            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" />100.00</div>
             </div>
           </div>
           <hr class="my-3" />
@@ -62,7 +62,7 @@
             </div>
             <div>
             	<div class="text-xs text-gray-600 text-right">Top bid</div>
-            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" /> 100.00</div>
+            	<div class="bids font-semibold text-sm text-right"><img class="inline-block mr-1 mb-1" src="~/static/images/bids.svg" />100.00</div>
             </div>
           </div>
           <hr class="my-3" />
@@ -71,7 +71,7 @@
       </div>
     </div>
 
-    <div class="discover px-32 pt-20 pb-48 mx-auto my-20 w-full bg-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Discover.png); max-width: 1003px">
+    <div class="discover px-16 md:px-32 py:12 md:pt-20 md:pb-48 mx-20 lg:mx-auto my-20 w-full bg-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Discover.png); max-width: 1003px">
       <div class="title text-white text-center text-3xl sm:text-4xl md:text-5xl leading-tight font-bold mb-10">
         Incorporate real estate assets, grow your land´s value
       </div>
@@ -81,12 +81,12 @@
       </div>
     </div>
 
-    <div class="rectangle px-24 py-20 w-full mx-auto my-24 bg-right-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Rectangle.png); max-width: 1003px">
+    <div class="rectangle py:12 md:py-20 px-16 md:pl-24 md:pr-64 w-full mx-auto my-20 bg-right-bottom bg-cover rounded-lg bg-no-repeat" style="background-image: url(/images/Rectangle.png); max-width: 1003px">
       <div class="title text-white text-3xl sm:text-4xl md:text-5xl leading-tight font-bold mb-10">
         Owner of the assets you buy,<br />
         thanks to NFT´s blockchain.
       </div>
-      <div class="text text-white" style="padding-right: 45%;">
+      <div class="text text-white md:mr-64">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec semper lacus. Nulla facilisi. In hac habitasse platea dictumst. Us et netus et malesuada fames ac turpis egestas</p>
         <p>Proin auctor enim et mi vestibulum pretium.</p>
       </div>
