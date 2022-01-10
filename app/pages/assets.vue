8bdc675d5c9a1f3f3109db9bb5331908fa6f245d
bot11x11 <procom@mail.ru>
2022-01-10 17:05:28 +0700

    buttons

--- app/pages/assets.vue
+++ app/pages/assets.vue
@@ -181,8 +181,12 @@
             <div class="font-medium text-xs mr-4">2k view</div>
             <div class="font-medium text-xs">100 favorites</div>
           </div>
-          <div class="text-xs">
+          <div class="text-xs leading-4 my-4">
             <p>This Parisian building with shops is a high end, photorealistic 3D model, that is created to help you add the realism to your project…</p>
+          </div>
+          <div class="flex text-xs">
+          	<a class="w-32 mr-2 p-4 border-white bg-white hover:bg-transparent" href="#">Buy Now</a>
+          	<a class="w-32 p-4 border-white bg-transparent hover:bg-white" href="#">Place a Bid</a>
           </div>
         </div>
       </div>
